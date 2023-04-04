5054616b11854acd0a5b6799c536333301d53996
Dor Shinar <dors718@gmail.com>
2023-04-04 20:57:42 +0300

    fix db

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,38 +1,10 @@
-import { connection, db } from "@/db/db";
+import { db } from "@/db/db";
 import { items } from "@/db/schema";
-import { sql } from "drizzle-orm";
 import { Inter } from "next/font/google";
 
 const inter = Inter({ subsets: ["latin"] });
-
 const getItems = async () => {
-  try {
-    let items = await connection.execute(
-      "select `id`, `name`, `checked`, `recurring` from `items`"
-    );
-    console.log("database-js:");
-    console.log("🚀 ~ items:", items.rows);
-  } catch (e) {
-    console.error(e);
-  }
-
-  try {
-    console.log("ORM");
-    let _items = await db.select().from(items);
-    console.log("🚀 ~ _items:", _items);
-  } catch (e) {
-    console.error(e);
-  }
-  console.log("query");
-  try {
-    let _items = await db.execute(
-      sql`select \`id\`, \`name\`, \`checked\`, \`recurring\` from \`items\``
-    );
-    console.log("🚀 ~ _items:", _items.rows);
-    return items;
-  } catch (e) {
-    console.error(e);
-  }
+  return db.select().from(items);
 };
 
 export default async function Home() {
@@ -40,9 +12,9 @@ export default async function Home() {
 
   return (
     <div>
-      {/* {items.rows.map((item) => (
+      {items.map((item) => (
         <div key={item.id}>{item.name}</div>
-      ))} */}
+      ))}
     </div>
   );
 }
