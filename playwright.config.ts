1c26f5e0cabdf90abfe5f20f4ae0ed56abe1a8bf
Dor Shinar <dors718@gmail.com>
2023-04-11 12:07:24 +0300

    CI

--- playwright.config.ts
+++ playwright.config.ts
@@ -1,10 +1,13 @@
 import { defineConfig, devices } from "@playwright/test";
+import { config } from "dotenv";
 
 /**
  * Read environment variables from file.
  * https://github.com/motdotla/dotenv
  */
-// require('dotenv').config();
+if (!process.env.CI) {
+  config({ path: ".env.local" });
+}
 
 /**
  * See https://playwright.dev/docs/test-configuration.
@@ -20,7 +23,7 @@ export default defineConfig({
   /* Opt out of parallel tests on CI. */
   workers: process.env.CI ? 1 : undefined,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
-  reporter: "html",
+  reporter: process.env.CI ? "dot" : "list",
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     /* Base URL to use in actions like `await page.goto('/')`. */
