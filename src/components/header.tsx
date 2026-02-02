9d767cf27ca58f3b708eee267d5429aa3f913069
dorshinar <dors718@gmail.com>
2026-02-02 17:42:43 +0200

    update deps

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -5,7 +5,9 @@ export function Header({ children }: React.PropsWithChildren) {
   return (
     <Link href="/">
       <h1 className="flex bg-white/80 backdrop-blur-sm p-4 text-xl font-semibold shadow-sm dark:bg-slate-800/80 border-b border-slate-200 dark:border-slate-700">
-        <span className="rounded-sm px-2 text-slate-700 dark:text-slate-50">{children}</span>
+        <span className="rounded-sm px-2 text-slate-700 dark:text-slate-50">
+          {children}
+        </span>
       </h1>
     </Link>
   );
